959d28925b699b0b77274387d75fe036ab5ee92a
krishpranav <krisna.pranav@gmail.com>
2021-05-24 16:00:36 +0530

    [+] check target functions [+]

---
+++ lib/check.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports
+
+from lib.output import *
+try:
+    from urllib.parse import urlparse
+except ImportError as e:
+    from urlparse import urlparse
+
+def checkTarget(target):
+    o = urlparse(target)
+    if o.netloc == "":
+        if "www." in o.path: return o.path.split('www.')[1]
+        return o.path
+    elif o.netloc != "":
+        if "www." in o.netloc: return o.netloc.split("www.")[1]
+        return o.netloc
+    else: return target
