5ff2603fd337b2500bbfbc2ff2e75678171a5c7f
krishpranav <krisna.pranav@gmail.com>
2021-05-25 14:03:41 +0530

    [+] update functions for searching email in ask.com [+]

---
+++ recon/ask.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports
+
+from lib.output import *
+from lib.request import *
+from lib.parser import *
+
+class Ask(Request):
+    def __init__(self, target):
+        Request.__init__(self)
+        self.target = target
+
+    def search(self):
+        test('Searching %s in Ask...'%(self.target))
+        url = "http://www.ask.com/web?q=%40{target}&pu=100&page=0".format(
+            target=self.target)
+        try:
+            resp = self.send(
+                method = 'GET',
+                url = url
+            )
+            return self.getemail(resp.content, self.target)
+        except Exception as e:
+            print(e)
+
+    def getemail(self, content, target):
+        return parser(content, target).email()
